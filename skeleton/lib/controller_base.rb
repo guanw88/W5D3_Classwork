6b86ce9a8e8ff06adbf6d20550cbdcc97ea3386f
guanw88 <guanw88@gmail.com>
2018-11-21 15:19:01 -0800

    Add #render method for ControllerBase

--- skeleton/lib/controller_base.rb
+++ skeleton/lib/controller_base.rb
@@ -32,7 +32,7 @@ class ControllerBase
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
-  def render_content(content, content_type)
+  def render_content(content, content_type="text/html")
     raise Error if already_built_response?
     @res['Content-Type'] = content_type
     @res.write(content)
@@ -42,6 +42,14 @@ class ControllerBase
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
+    dir_path = File.dirname(__FILE__)
+    parent_dir = "/" + dir_path.split("/")[0..-2].join("/")
+    
+    template_path = File.join(parent_dir, "views", "cats_controller","#{template_name}.html.erb") 
+    
+    file_content = File.read(template_path)
+    
+    render_content(ERB.new(file_content).result(binding))
   end
 
   # method exposing a `Session` object
