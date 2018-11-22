8a1c18f8fa719e4eabf2f051f6461f3f3c101a53
guanw88 <guanw88@gmail.com>
2018-11-21 17:32:36 -0800

    Add router methods

--- skeleton/lib/router.rb
+++ skeleton/lib/router.rb
@@ -25,10 +25,13 @@ class Router
   attr_reader :routes
 
   def initialize
+    @routes = []
   end
 
   # simply adds a new route to the list of routes
   def add_route(pattern, method, controller_class, action_name)
+    route = Route.new(pattern, method, controller_class, action_name)
+    @routes << route
   end
 
   # evaluate the proc in the context of the instance
@@ -39,13 +42,26 @@ class Router
   # make each of these methods that
   # when called add route
   [:get, :post, :put, :delete].each do |http_method|
+    define_method(http_method) do |pattern, controller_class, action_name|
+      add_route(pattern, http_method, controller_class, action_name)
+    end
   end
 
   # should return the route that matches this request
   def match(req)
+    @routes.each do |route|
+      return route if route.matches?(req)
+    end 
+    nil
   end
 
   # either throw 404 or call run on a matched route
   def run(req, res)
+    route = match(req)
+    if route
+      route.run(req,res)
+    else 
+      res.status = 404
+    end 
   end
 end
